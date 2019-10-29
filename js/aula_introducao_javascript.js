2717f42d483a5ab601247446781019a6cb4e2a1e
CLAUDIA AZEVEDO SANTOS <claudia.asantos36@senacsp.edu.br>
2019-10-29 11:30:15 -0300

    html bug removed (AGAIN)

--- js/aula_introducao_javascript.js
+++ js/aula_introducao_javascript.js
@@ -1,33 +1,44 @@
  //declara que serão usadas duas variáveis
 var dado01;
 var dado02;
+
+var farol;
 console.log('rtdfgxdfhfg');
 //coloca um valor aleatório nestas variáveis...
 //
-dado01 = Math.round(6 * Math.random());
-dado02 = Math.round(6 * Math.random());
+dado01 = Math.floor(10 * Math.random()) + 1;
+dado02 = Math.floor(10 * Math.random()) + 1;
 
-if (dado01 == 0) {
-	dado01++;
+farol = 0;
+
+function rolardados ()
+{
+	if(farol ==0 ){
+		if (dado01 > dado02){
+			document.getElementById("paragrafo03").innerHTML = "Jogador 1 ganhou";
+		}else if (dado01 < dado02){
+			document.getElementById("paragrafo03").innerHTML = "Jogador 2 ganhou";
+		}else {
+			document.getElementById("paragrafo03").innerHTML = "Empate!!!";
+		}
+
+		document.getElementById("p1-dice" + dado01).style.display = "inline-block";
+		document.getElementById("p2-dice" + dado02).style.display = "inline-block";
+
+		//prepend -> adiciona (nesse caso) uma string no início do elemeto//
+		document.getElementById("paragrafo01").prepend("Jogador 1: " + dado01); //mesma coisa q inner HTML = [texto] + innerHTML  (adiciona no início do html interno)
+		document.getElementById("paragrafo02").prepend("Jogador 2: " + dado02);
+		farol = 1;
+	} else{
+		window.location.reload()
+	}
+
 }
-if (dado02 == 0) {
-	dado02++;
-}
 
-if (dado01 > dado02){
-	document.getElementById("paragrafo03").innerHTML = "Jogador 1 ganhou";
-}else if (dado01 < dado02){
-	document.getElementById("paragrafo03").innerHTML = "Jogador 2 ganhou";
-}else {
-	document.getElementById("paragrafo03").innerHTML = "Empate!!!";
-}
+document.getElementById("rolar").addEventListener("click", rolardados);
 
-document.getElementById("p1-dice" + dado01).style.display = "inline-block";
-document.getElementById("p2-dice" + dado02).style.display = "inline-block";
 
-//prepend -> adiciona (nesse caso) uma string no início do elemeto//
-document.getElementById("paragrafo01").prepend("Jogador 1: " + dado01); //mesma coisa q inner HTML = [texto] + innerHTML  (adiciona no início do html interno)
-document.getElementById("paragrafo02").prepend("Jogador 2: " + dado02);
+
 
 
 
